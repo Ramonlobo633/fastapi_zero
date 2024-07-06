d0214e88067a93f9069a1d420281f2276d132a30
Ramonlobo633 <ramonlobo633@gmail.com>
2024-07-06 11:08:42 -0300

    Adding migrations and settings

--- fast_zero/api/app.py
+++ fast_zero/api/app.py
@@ -2,14 +2,13 @@ from http import HTTPStatus
 
 from fastapi import FastAPI
 
-from fast_zero.routes import auth, todos, users
+from fast_zero.api.routes import auth, users
 from fast_zero.schemas import Message
 
 app = FastAPI()
 
 app.include_router(users.router)
 app.include_router(auth.router)
-app.include_router(todos.router)
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -3,7 +3,7 @@ from http import HTTPStatus
 
 def test_get_token(client, user):
     response = client.post(
-        '/auth/token',
+        'auth/token',
         data={'username': user.email, 'password': user.clean_password},
     )
     token = response.json()
